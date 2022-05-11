ac12e97ec4adeb01ab1f17cbbc8d83080fc19c42
cbarrios <lalobarrios16@yahoo.com>
2022-05-11 18:51:54 -0400

    master: (5) Hello Compose!

--- app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -16,13 +16,10 @@
 
 package com.google.samples.apps.sunflower.plantdetail
 
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun PlantDetailDescription() {
-    Surface {
-        Text("Hello Compose")
-    }
+    Text("Hello Compose")
 }
